4774f0fd5dbfaffac6506cea0016669d23be88a8
Rishik Chakravarty <rishikrc3@gmail.com>
2023-08-16 19:11:15 +0530

    basic_express_server

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
+const { log } = require("console");
+const fs = require("fs");
+const express = require("express");
+const app = express();
+const port = 3000;
+
+function handleRequest(req, res) {
+  res.send("Hello World!");
+}
+app.get("/", handleRequest);
+function started() {
+  console.log(`Example app listening on port ${port}`);
+}
+app.listen(port, started);
+
+function callbackFn(err, data) {
+  console.log(data);
+}
+fs.readFile("a.txt", "utf-8", callbackFn);
 function calculateSum(counter) {
   var sum = 0;
   for (var i = 0; i < counter; i++) {
